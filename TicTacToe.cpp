fbbd24b6a73b38d850f9051e8ed437355b26d683
lifeisbleak1 <58531858+lifeisbleak1@users.noreply.github.com>
2020-07-30 15:45:04 -0400

    Update TicTacToe.cpp
    Fully functional

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -13,12 +13,14 @@ string grid[4][4] = {
 		};
 
 void displayGrid(){
+	cout << endl;
 	for(int i = 0; i <= 3; i++){ // iterates through rows
 		for(int j = 0; j <= 3; j++){ // iterates through columns
-			cout << grid[i][j]; // print cells on same line
+			cout << " " << grid[i][j]; // print cells on same line
 		}
 		cout << endl; // every time a row is finished add a new line
 	}
+	cout << endl;
 }
 
 void enterLetter(int turnCount){
@@ -55,23 +57,66 @@ void enterLetter(int turnCount){
 }
 
 void checkWin(){
-	//rows
-	for(int i = 0; i<=3; i++){
-		if("O" == grid[i][1] && "O" == grid[i][2] && "O" == grid[i][3]){
-			cout << "O Wins!" << endl;
-			gameOver = true;
+	string winner[2] = {"O", "X"};
+	for(int i = 0; i < 2; i++){
+		for(int j = 1; j <= 3; j++){ // check rows
+			if(grid[j][1] == winner[i] && grid[j][2] == winner[i] && grid[j][3] == winner[i]){
+				displayGrid();
+				cout << "\n" << winner[i] << " Wins!\n" << endl;
+				cout << "3 across, row " << j << endl;
+				gameOver = true;
+			}
+		}
+		for(int k = 1; k <= 3; k++){ // check columns
+			if(grid[1][k] == winner[i] && grid[2][k] == winner[i] && grid[3][k] == winner[i]){
+				displayGrid();
+				cout << "\n" << winner[i] << " Wins!\n" << endl;
+				cout << "3 down, column " << k << endl;
+				gameOver = true;
+			}
+		}
+		if(grid[1][1] == winner[i] && grid[2][2] == winner[i] && grid[3][3] == winner[i]){
+				displayGrid();
+				cout << "\n" << winner[i] << " Wins!\n" << endl;
+				cout << "Diagonal" << endl;
+				gameOver = true;
+		}
+		if(grid[1][3] == winner[i] && grid[2][2] == winner[i] && grid[3][1] == winner[i]){
+				displayGrid();
+				cout << "\n" << winner[i] << " Wins!\n" << endl;
+				cout << "Reverse diagonal" << endl;
+				gameOver = true;
 		}
 	}
 }
 
-int main(){
+void resetGame(){
+	turnCount = 0;
+	gameOver = false;
+	for(int i = 1; i<=3; i++){
+		for(int j = 1; j<=3; j++){
+			grid[i][j] = "_";
+		}
+	}
 
+}
+
+int main(){
 	while(!gameOver){
 		displayGrid();
 		enterLetter(turnCount);
 		checkWin();
 		turnCount++;
+		cout << "\n----------------" << endl; // 16 dashes
 	}
-
+	string yesNo;
+	cout << "Would you like to play again? (y/n)\n";
+	cin >> yesNo;
+	if(yesNo == "y"){
+		resetGame();
+		main();
+	}else{
+		cout << "guess you don't like fun then\n" << endl;
+	}
 	return 0;
 }
